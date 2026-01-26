48897576dd21709f0c10614f6191fcaa6a070033
arato-make <arato.make@gmail.com>
2026-01-27 00:08:31 +0900

    gwm v0.1.5

--- gwm.rb
+++ gwm.rb
@@ -1,20 +1,20 @@
 class Gwm < Formula
   desc "git worktree manager"
   homepage "https://github.com/arato-make/gwm"
-  version "0.1.4"
+  version "0.1.5"
 
   on_macos do
     on_intel do
-      url "https://github.com/arato-make/gwm/releases/download/v0.1.4/gwm_Darwin_x86_64.tar.gz"
-      sha256 "48eeb79b1dfa46a36d4899193babd436ea520272253e168740a7d376e606d73b"
+      url "hhttps://github.com/arato-make/gwm/releases/download/v0.1.5/gwm_Darwin_x86_64.tar.gz"
+      sha256 "e297355a49442a831c145592fc73d9ec893076b02479e331f8a6dcacb6765375"
 
       def install
         bin.install "gwm"
       end
     end
     on_arm do
-      url "https://github.com/arato-make/gwm/releases/download/v0.1.4/gwm_Darwin_arm64.tar.gz"
-      sha256 "911056de7031d2ef1a89a2ea40489dd23c94d0d60693e8a5b5ff495627806cc7"
+      url "https://github.com/arato-make/gwm/releases/download/v0.1.5/gwm_Darwin_arm64.tar.gz"
+      sha256 "9a40d7be41ffff19d84c23b3b26bc4103611582a73052d415fb88f33609e2d3b"
 
       def install
         bin.install "gwm"
